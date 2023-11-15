36b62d633e4977039253ce7d6403a95de1a128cd
bryanaldridgejr <bryanaldridgejr@gmail.com>
2023-11-15 15:07:10 -0600

    push file

---
+++ main.js
@@ -0,0 +1,70 @@
+let arrayOfPosts;
+let arrayOf5Post;
+let arrayOfUsers;
+let arrayOfComments;
+// This function waits for the web page to be loaded, when it does it will run the code inside of it which happens to be getPosts()
+window.onload = function() {
+  getPosts()
+
+}
+
+// This function is going to make a fetch request to the URL inside its parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
+const getPosts = () => {
+  fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(posts => arrayOfPosts = posts)
+}
+
+// This function logs the results in your browser's console
+const consolePosts = () => {
+  console.log(arrayOfPosts)
+}
+
+// this function creates elements inside the all-posts ul, then appends text inside it with the posts that were returned in the request.
+const displayPost = () => {
+  const allPosts = document.getElementById('all-posts')
+  arrayOfPosts.map((post, index) => {
+    const li = document.createElement('li')
+    const text = document.createTextNode(`#${index}, Title: ${post.title}:  ${post.body}, By user: ${post.userId}`)
+    li.appendChild(text)
+    allPosts.append(li)
+  })
+}
+
+
+const display5Posts = () => {
+  const allPosts1 = document.getElementById('all-posts1')
+  const filteredPost = arrayOfPosts.slice(0, 5);
+  filteredPost.forEach((post) => {
+    const li = document.createElement('li');
+    const text = document.createTextNode(`Title: ${post.title}:  ${post.body}, By user: ${post.userId}`);
+    li.appendChild(text);
+    allPosts1.append(li);
+  })
+}
+
+const displayComments = () => {
+  const allPosts2 = document.getElementById('all-posts2')
+  arrayOfPosts.map((post) => {
+    const li = document.createElement('li')
+    const text = document.createTextNode(`${post.body}`)
+    li.appendChild(text)
+    allPosts2.append(li)
+  })
+}
+
+const displayUsers = () => {
+  const allPosts3 = document.getElementById('all-posts3')
+  arrayOfPosts.map((post) => {
+    const li = document.createElement('li')
+    const text = document.createTextNode(`user: ${post.userId}`)
+    li.appendChild(text)
+    allPosts3.append(li)
+  })
+}
+/* 
+Your job now is to follow the functions above and use them as templates 
+ to build the functionality the buttons in the index.html file already 
+ have laid out in it. This way you can learn how to build fetch requests 
+ and work with other APIs and become a real developer!! 
+*/
